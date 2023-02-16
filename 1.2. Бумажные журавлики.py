d20e41e34ed2537dac15349bf85cce1505034d01
Senhaizer <113827473+Senhaizer@users.noreply.github.com>
2023-02-16 17:59:05 +0300

    Create 1.2. Бумажные журавлики.py

---
+++ 1.2. Бумажные журавлики.py
@@ -0,0 +1,7 @@
+cranes = int(input("Введите общее количество журавликов: "))
+
+Kate = int((cranes/3)*2)
+Peter = int((Kate/2)/2)
+Sergey = int(Peter)
+
+print(f"Всего журавликов было сделано {cranes}, из которых Петя сделал {Peter}, Катя сделала {Kate}, а Сережа сделал {Sergey}.")
