c53c4e406dce33d8dfc07ab5469ba63d7448f620
Senhaizer <113827473+Senhaizer@users.noreply.github.com>
2023-03-10 16:56:52 +0300

    Create User_interface_module.py

---
+++ HW7/7.2./User_interface_module.py
@@ -0,0 +1,25 @@
+def get_user_data_check(msg_1: str, msg_2: str, count: int) -> tuple[int, int]:
+    count += 1
+    print(f"{msg_1}{count}{msg_2}", end='')
+
+    while (True):
+        try:
+            user_input = int(input())
+            if (type(user_input) == int):
+                result = (user_input, count)
+                return (result)
+            else:
+                raise ValueError
+        except ValueError:
+            print("Введенное значение не соответствует условиям задачи, повторите ввод.")
+
+def get_user_data(messages: tuple[str]) -> tuple[int]:
+    user_data_list = []
+    number_of_messages = len(messages)
+    for i in range(number_of_messages):
+        print()
+        user_data = int(input(messages[i]))
+        user_data_list.append(user_data)
+
+    result = user_data_list
+    return result
